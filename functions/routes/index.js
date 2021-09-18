78b8554ead62877c67542a8512745319ea166dc9
Harsh Patel <contact@harshpatel.ca>
2021-09-17 22:24:20 -0400

    App health testing done

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+/* GET status. */
+router.get("/status", function (req, res, next) {
+  res.status(200).send({ message: "ok" });
+});
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Expresss" });
--- functions/test/test.js
+++ functions/test/test.js
@@ -1,7 +1,14 @@
 const assert = require("assert");
 const app = require("../app");
+const expect = require("chai").expect;
+const request = require("supertest");
 
 describe("App", function () {
+  it("App Health Status", async function () {
+    const response = await request(app).get(`/status`);
+    expect(response.status).to.equal(200);
+    expect(response.body.message).to.equal("ok");
+  });
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
