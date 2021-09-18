953f0331e9c5e708e26d5a5db2f0e7f940054470
Harsh Patel <contact@harshpatel.ca>
2021-09-17 22:47:27 -0400

    Added image service testing

--- functions/test/test.js
+++ functions/test/test.js
@@ -3,15 +3,33 @@ const app = require("../app");
 const expect = require("chai").expect;
 const request = require("supertest");
 
-describe("App", function () {
+describe("Dropzone API", function () {
   it("App Health Status", async function () {
     const response = await request(app).get(`/status`);
     expect(response.status).to.equal(200);
     expect(response.body.message).to.equal("ok");
   });
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+  describe("Image Service", function () {
+    let uploadImageID = "";
+    it("Image Service Health Status", async function () {
+      const response = await request(app).get(`/images/status`);
+      expect(response.status).to.equal(200);
+    });
+
+    it("Upload Image to Server", async function () {
+      this.timeout(4000);
+      const imageContent = {
+        image: "test/sample.jpeg",
+      };
+      const response = await request(app).post(`/images`).send(imageContent);
+      expect(response.status).to.equal(200);
+      uploadImageID = response.body.public_id;
+    });
+
+    it("Delete Image from Server", async function () {
+      this.timeout(3000);
+      const response = await request(app).delete(`/images/${uploadImageID}`);
+      expect(response.status).to.equal(200);
     });
   });
 });
